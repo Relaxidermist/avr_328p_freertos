b2e4560bf037e97ee9867282b0274b3818349890
ingram weeks <ingram.weeks@gmail.com>
2023-04-11 14:41:19 +0100

    removed function led task function def in main

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  *      Author: jcobb
  */
 #include <avr/io.h>
-//#include "apptasks.h"
+#include "apptasks.h"
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -13,21 +13,6 @@
 #define mainLED_TASK_PRIORITY 			(tskIDLE_PRIORITY+1)
 // #define mainNEXT_TASK_2				(tskIDLE_PRIORITY+2)
 // #define mainNEXT_TASK_3				(tskIDLE_PRIORITY+3)
-
-
-// TODO: Research moving vLEDFlashTask method to apptasks.c
-void vLEDFlashTask(void *pvParms)
-{
-	vLEDInit();
-	portTickType xLastWakeTime;
-	const portTickType xFrequency = 100;
-	xLastWakeTime = xTaskGetTickCount();
-
-	for(;;) {
-		vLEDToggle();
-		vTaskDelayUntil(&xLastWakeTime, xFrequency);
-	}
-}
 
 void init(void);
 
