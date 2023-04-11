1dbfe195ff7d890631f214f9ffbb8736fdd34935
ingram weeks <ingram.weeks@gmail.com>
2023-04-11 15:26:32 +0100

    created basic usart function and included usart code from https://www.coderoasis.com/implementing-uart-on-the-atmega328p/  updated functions to reflect MISRA style

--- Drivers/usart.h
+++ Drivers/usart.h
@@ -3,15 +3,19 @@
  *
  *  Created on: 11 Apr 2023
  *      Author: ingram
+ *
+ *      usart code stolen from here:
+ *      https://www.coderoasis.com/implementing-uart-on-the-atmega328p/
+ *
  */
 
 #ifndef DRIVERS_USART_H_
 #define DRIVERS_USART_H_
 
-void usart_set_message(char* message);
+void vUSARTSetMessage(char* message);
 
-void usart_init();
+void vUSARTInit();
 
-void usart_print();
+void vUSARTPrint();
 
 #endif /* DRIVERS_USART_H_ */
